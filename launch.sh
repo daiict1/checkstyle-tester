babd1391546273aaa22078151170c1613ebd8839
Alexey <zuy_alexey@mail.ru>
2014-11-27 23:34:53 +0300

    configured for testing UselessSingleCatchCheck

--- launch.sh
+++ launch.sh
@@ -33,7 +33,9 @@ else
 fi
 
 echo "Running Checkstyle on $SOURCES_DIR ..."
-time mvn checkstyle:check "$@" > target/console_log.txt
+time mvn site "$@" > target/console_log.txt
+#time mvn checkstyle:checkstyle  "$@" > target/console_log.txt
+#time mvn checkstyle:check  "$@" > target/console_log.txt
 echo "Running Checkstyle on $SOURCES_DIR - finished"
 
 echo "Testing Checkstyle finished"
